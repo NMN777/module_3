9c35e319b18b1c62c17b7f36bac8553f07c6438f
NMN777 <nabokovamn@gmail.com>
2021-11-28 22:49:54 +0300

    add final task

---
+++ final/metrics.py
@@ -0,0 +1,45 @@
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    # https://towardsdatascience.com/accuracy-precision-recall-or-f1-331fb37c5cb9
+
+    accuracy = sum(prediction == ground_truth) / len(prediction)
+
+    true_positive = sum((prediction == True) & (ground_truth == True))
+    false_positive = sum((prediction == True) & (ground_truth == False))
+    false_negative = sum((prediction == False) & (ground_truth == True))
+
+    precision = true_positive / (true_positive + false_positive)
+    recall = true_positive / (true_positive + false_negative)
+    f1 = 2 * (precision * recall) / (precision + recall)
+    
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+
+    return sum(prediction == ground_truth) / len(prediction)
